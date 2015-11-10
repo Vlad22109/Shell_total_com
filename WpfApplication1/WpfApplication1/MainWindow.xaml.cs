93a17b714d0deeb63b56be8631cb4bacc86462a1
Vlad <Vlad22109@gmail.com>
2015-11-10 14:59:07 +0200

    Gotova 2

--- WpfApplication1/WpfApplication1/MainWindow.xaml.cs
+++ WpfApplication1/WpfApplication1/MainWindow.xaml.cs
@@ -1,22 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Management;
-using System.Globalization;
-using System.ComponentModel;
-using System.Drawing;
+﻿using System.Windows;
 namespace WpfApplication1
 {
 /// <summary>
@@ -24,41 +6,7 @@ namespace WpfApplication1
 /// </summary>
 public partial class MainWindow : Window
 {
-    private TreeView tvFolders;
-		private ListView lvFiles;
-        private IContainer components;
-        private Menu mainMenu1;
-        private MenuItem menuItem1;
-        private MenuItem menuItem2;
-        private MenuItem menuItem3;
-        private MenuItem menuItem4;
-        private ContextMenu treeContextMenu;
-        private ContextMenuEventArgs miCopy;
-        private ContextMenuEventArgs miPastle;
-        private ContextMenu listContextMenu;
-        private ContextMenuEventArgs lvCopy;
-        private ContextMenuEventArgs lvPastle;
-        private ContextMenu dragContextMenu;
-        private ContextMenuEventArgs miMove;
-        private ContextMenuEventArgs miDropCopy;
-    
-	public MainWindow()
-		{
-			InitializeComponent();
-            PopulateDriveList();
-			
-		}
-   
-   
-    private void PopulateDriveList()
-    {
-      
-       
-    }
-
-
-}}
-		
-		
-		
-
+public MainWindow()
+{
+InitializeComponent();
+}}}
